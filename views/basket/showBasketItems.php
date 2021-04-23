70f2b8c6059268671aa34171aa20a806aa4d5b0b
Bhbrk <bogdankrut12@gmail.com>
2021-04-23 18:10:08 +0300

    do adjust for small screens in showBasketItems page in mobileFirstApproach styles

--- views/basket/showBasketItems.php
+++ views/basket/showBasketItems.php
@@ -12,7 +12,7 @@
     foreach ($params['items'] as $item):
         ?>
         <div class="row basketSingleItemContainer<?=$item['id']?> text-center">
-            <div class="col-sm-6 col-md-12">
+            <div class="singleBasketElementContainer col-md-12">
                 <h5 class="card-title"><?= $item['name'] ?></h5>
                 <img class="basketSingleImage img-fluid me-3" src="/template/images/<?= $item['photo'] ?>" alt="">
 
@@ -20,13 +20,13 @@
                 <a class="minusItemsCount<?=$item['id']?> me-2" href="/clientBasket/showBasketItems/<?=$prev?>"><i class="fa fa-minus"></i></a>
                 <input type="hidden" name="itemId" class="itemId<?=$item['id']?>" value="<?=$item['id']?>">
 <!--                <input class="basketItemCountInput" value="1" min="1" type="range">-->
-                <input type="text" class="showBasketItemCountInput<?=$item['id']?> form-control-sm">
+                <input type="text" class="showBasketItemCountInput<?=$item['id']?> singleBasketCountInput form-control-sm">
                 <a class="plusItemsCount<?=$item['id']?>" href=""><i class="fa fa-plus"></i></a>
 
                 <div class="d-inline-block ms-5">
                     <h5 class="basketItemPrice<?=$item['id']?> d-inline-block pe-3"><?=$item['price']?>$</h5>
-                    <a href="#" class="btn btn-success">Submit buy</a>
-                    <a href="/clientBasket/showBasketItems/1" class="deleteBasketItem<?=$item['id']?> btn btn-danger ms-lg-3">Delete</a>
+                    <a href="#" class="singleSubmitBuyButton btn btn-success">Submit buy</a>
+                    <a href="/clientBasket/showBasketItems/1" class="singleDeleteBasketItemButton deleteBasketItem<?=$item['id']?> btn btn-danger ms-lg-3">Delete</a>
                 </div>
 
             </div>
